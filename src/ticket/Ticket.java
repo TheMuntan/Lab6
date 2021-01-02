564e5056daebffd22b98ab783e043364159c9d9b
Muntan <serdar_tst@hotmail.com>
2021-01-02 21:41:28 +0100

    worked on ticket class

--- src/ticket/Ticket.java
+++ src/ticket/Ticket.java
@@ -1,4 +1,24 @@
 package ticket;
 
+import person.Person;
+
 public class Ticket {
+
+    private Person[] pList;
+    private boolean evenSplit;
+
+    public Ticket(Person[] pList, boolean evenSplit) {
+        this.pList = pList;
+        this.evenSplit = evenSplit;
+    }
+
+    public Person[] getpList() {
+        return pList;
+    }
+
+    public boolean isEvenSplit() {
+        return evenSplit;
+    }
+
+    
 }
