b3e0f5b5b27748110f3f03b36fda54ec8bbffb2b
Muntan <serdar_tst@hotmail.com>
2021-01-10 16:13:22 +0100

    test

---
+++ src/test/Calculator_UTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+
+import person.Person;
+
+public class Calculator_UTest {
+
+    private List<Person> pList = new ArrayList<>();
+
+    @Test
+    public void test(List<Person> pList) {
+
+        
+    }
+    
+}
