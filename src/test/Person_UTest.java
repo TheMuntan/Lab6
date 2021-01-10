f34bbc2dd9f0996ffae86cd5bf81c4d5686b2484
TheMuntan <serdar_tst@hotmail.com>
2021-01-10 18:12:51 +0100

    unit test

--- src/test/Person_UTest.java
+++ src/test/Person_UTest.java
@@ -13,23 +13,48 @@ import person.Person;
 public class Person_UTest {
 
     @Test
-    public void test() {
+    public void getOwedListTest() {
 
-        AbstractFactory pFactory = FactoryProducer.getFactory("person");
-        Person serdar = pFactory.getPerson("Serdar");
-        Person luffy = pFactory.getPerson("Luffy");
-        Person broly = pFactory.getPerson("Broly");
+        Person serdar = new Person("Serdar");
+        Person luffy = new Person("Luffy");
+        Person broly = new Person("Broly");
+
         serdar.owedBy(luffy, 34.24);
         serdar.owedBy(luffy, 36.76);
         serdar.owedBy(broly, 153.0);
         serdar.owedBy(broly, 452.0);
-        HashMap<Person, Double> correctHMap = new HashMap<>();
+
+        HashMap<Person, Double> correctHMap = new HashMap<>(); //expected output
         correctHMap.put(luffy, 71.00);
         correctHMap.put(broly, 605.00);
 
-        Assert.assertEquals(correctHMap, serdar.getOwedList(), "Testing the person class - should return HashMap of owed people");
-
+        Assert.assertEquals(correctHMap, serdar.getOwedList());
 
     }
-    
+
+    @Test
+    public void setOwedListTest() {
+
+        Person serdar = new Person("Serdar");
+        Person luffy = new Person("Luffy");
+        Person broly = new Person("Broly");
+
+        HashMap<Person, Double> correctHMap = new HashMap<>(); //expected output
+        correctHMap.put(luffy, 71.00);
+        correctHMap.put(broly, 605.00);
+
+        serdar.setOwedList(correctHMap);
+
+
+        Assert.assertEquals(correctHMap, serdar.getOwedList());
+
+    }
+
+    @Test
+    public void getNameTest() {
+        Person serdar = new Person("Serdar");
+
+        Assert.assertEquals("Serdar", serdar.getName());
+    }
+
 }
--- src/test/integration_test.java
+++ src/test/integration_test.java
@@ -1,9 +1,31 @@
 package test;
 
+import controller.PersonController;
+import factory.AbstractFactory;
+import factory.FactoryProducer;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import person.Person;
+import view.PersonView;
 
 public class integration_test {
 
+
+    //testing the integration between the FactoryProducer, AbstractFactory and the MVC classes
+    @Test
+    public void factoryMVC_integration_test() {
+
+        AbstractFactory pFactory = FactoryProducer.getFactory("person");
+        Person person = pFactory.getPerson("Person");
+        PersonView pView = new PersonView();
+
+        PersonController personController = new PersonController(person, pView);
+
+        Assert.assertEquals("Person", personController.getPersonName());
+        personController.setPersonName("newName");
+        Assert.assertEquals("newName", personController.getPersonName());
+
+
+    }
 
 }
